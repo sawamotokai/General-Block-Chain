4c77ba3272605317829ceb8a5c2f1efcc757b6a7
Kai Sawamoto <kaisawamoto@gmail.com>
2020-01-03 09:16:15 -0800

    create_block

--- models/BlockChain.js
+++ models/BlockChain.js
@@ -1,5 +1,21 @@
 class BlockChain {
 	constructor() {
 		this.chain = [];
+		this.create_block(1, '0');
+	}
+
+	create_block(proof, prev_hash) {
+		let block = {
+			index: this.chain.length + 1,
+			timestamp: new Date().toISOString(),
+			proof: proof,
+			prev_hash: prev_hash
+		};
+		this.chain.push(block);
+
+		return JSON.stringify(block);
 	}
 }
+
+const blockChain = new BlockChain();
+console.log(blockChain.create_block(2, 'fa3113f'));
