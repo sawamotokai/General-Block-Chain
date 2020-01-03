009fc68806ebe334312f25b21ae383effad99f01
Kai Sawamoto <kaisawamoto@gmail.com>
2020-01-03 10:01:12 -0800

    findNonce func done

--- models/BlockChain.js
+++ models/BlockChain.js
@@ -1,3 +1,5 @@
+const sha256 = require('js-sha256').sha256;
+
 class BlockChain {
 	constructor() {
 		this.chain = [];
@@ -11,11 +13,26 @@ class BlockChain {
 			proof: proof,
 			prev_hash: prev_hash
 		};
-		this.chain.push(block);
-
+		this.chain.push(JSON.stringify(block));
 		return JSON.stringify(block);
+	}
+
+	get last_block() {
+		return this.chain.slice(-1)[0];
+	}
+
+	findNonce(prev_nonce) {
+		let nonce = 0;
+		let isValidNonce = false;
+		while (!isValidNonce) {
+			let hash = sha256((pow(nonce, 2) - pow(prev_nonce, 2)).toString());
+			if (hash.slice(0, 4) === '0000')
+				isValidNonce = true; // mining successes if leading 4 digits are 0
+			else nonce++;
+		}
+		return nonce;
 	}
 }
 
-const blockChain = new BlockChain();
-console.log(blockChain.create_block(2, 'fa3113f'));
+let BC = new BlockChain();
+console.log(BC.findNonce(31));
