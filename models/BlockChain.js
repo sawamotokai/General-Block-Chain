77920dc52fe0f8898133ff2050657394cc1e6d16
Kai Sawamoto <kaisawamoto@gmail.com>
2020-01-03 18:05:26 -0800

    added stringfy with sorted key & isChainValid

--- models/BlockChain.js
+++ models/BlockChain.js
@@ -1,4 +1,5 @@
 const sha256 = require('js-sha256').sha256;
+const stringify = require('json-stable-stringify');
 
 class BlockChain {
 	constructor() {
@@ -6,15 +7,15 @@ class BlockChain {
 		this.create_block(1, '0');
 	}
 
-	create_block(proof, prev_hash) {
+	create_block(nouce, prev_hash) {
 		let block = {
 			index: this.chain.length + 1,
 			timestamp: new Date().toISOString(),
-			proof: proof,
+			nouce: nouce,
 			prev_hash: prev_hash
 		};
-		this.chain.push(JSON.stringify(block));
-		return JSON.stringify(block);
+		this.chain.push(block);
+		return stringify(block);
 	}
 
 	get last_block() {
@@ -31,6 +32,42 @@ class BlockChain {
 			else nonce++;
 		}
 		return nonce;
+	}
+	// findNonce(prev_hash) {
+	// 	let nonce = 0;
+	// 	let block = stringify({
+	// 		index: this.chain.length + 1,
+	// 		timestamp: new Date().toISOString(),
+	// 		nouce: nouce,
+	// 		prev_hash: prev_hash
+	// 	});
+	// 	let isValidNonce = false;
+	// 	while (!isValidNonce) {
+	// 		let hash = sha256(block);
+	// 		if (hash.slice(0, 4) === '0000')
+	// 			isValidNonce = true; // mining successes if leading 4 digits are 0
+	// 		else block.nonce++;
+	// 	}
+	// 	return [ block ];
+	// }
+
+	hash(block) {
+		return sha256(stringify(block));
+	}
+
+	// loop over a chain to see all the links are valid
+	isChainValid(chain) {
+		if (chain.length == 1) return true;
+		let prev_block = chain[1];
+		for (let i = 2; i < chain.lenght; i++) {
+			let cur_block = chain[i];
+			if (cur_block.prev_hash !== this.hash(prev_block)) return false;
+			let prev_nonce = prev_block.nouce;
+			let cur_nonce = cur_block.nouce;
+			if (sha256((Math.pow(cur_nonce, 2) - Math.pow(prev_nonce, 2)).toString()).slice(0, 4) !== '0000') return false;
+			prev_block = cur_block;
+		}
+		return true;
 	}
 }
 
